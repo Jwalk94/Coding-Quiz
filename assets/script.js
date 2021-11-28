8b53278ed9314c51463e88c73d62802b676470c3
Jwalk94 <92691097+Jwalk94@users.noreply.github.com>
2021-11-28 15:56:27 -0500

    adding files needed and basic setup for each

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+//Create array for questions
+var questions = [
+    {
+        quest: "What tag is ised to define a hyperlink, or a link to another page:",
+        choices: ["<strong>", "<em>", "<a>", "<link>"],
+        answer: "<a>"
+    },
+]
